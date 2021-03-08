6d4e42b3a17f8b9ca5bff16fce018c19823d1f1c
Pavel Darii <paveldarii@yahoo.com>
2021-03-08 00:56:33 -0500

    moved pages folder from the components directly in the src folder

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+function Home() {
+  return (
+    <div id="home-page">
+      <Hero></Hero>
+    </div>
+  );
+}
+
+export default Home;
