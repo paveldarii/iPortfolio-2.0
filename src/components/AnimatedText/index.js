ce096adf97baba3fb7cf5ec04bf8100219ad899d
Pavel Darii <paveldarii@yahoo.com>
2021-03-08 00:55:08 -0500

    created a component that animates the list of web technologies

---
+++ src/components/AnimatedText/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./style.css";
+
+const AnimatedText = () => {
+  return (
+    <div className="Iam">
+      <b>
+        <div className="innerIam">
+          JavaScript
+          <br />
+          HTML
+          <br />
+          CSS
+          <br />
+          Bootstrap
+          <br />
+          MongoDB
+          <br />
+          SQL
+          <br />
+          React
+          <br />
+          Jest
+          <br />
+          WebPack
+          <br />
+          And Other ...
+        </div>
+      </b>
+    </div>
+  );
+};
+
+export default AnimatedText;
