9e88be3f14782210fffa2f86cda4e43e331ede6d
Pavel Darii <paveldarii@yahoo.com>
2021-03-07 21:05:31 -0500

    added portfolio and contact page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-import Blog from "./components/pages/Blog";
+import Resume from "./components/pages/Resume";
 import Contact from "./components/pages/Contact";
+import Portfolio from "./components/pages/Portfolio";
 
 function App() {
   return (
@@ -13,8 +14,9 @@ function App() {
         <NavTabs />
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
-        <Route exact path="/blog" component={Blog} />
-        <Route path="/contact" component={Contact} />
+        <Route exact path="/resume" component={Resume} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/portfolio" component={Portfolio} />
       </div>
     </Router>
   );
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -10,40 +10,51 @@ function NavTabs() {
   return (
     <ul className="nav nav-tabs">
       <li className="nav-item">
-        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+        <Link
+          to="/"
+          className={location.pathname === "/" ? "nav-link active" : "nav-link"}
+        >
           Home
         </Link>
       </li>
       <li className="nav-item">
         <Link
           to="/about"
-          className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
+          className={
+            location.pathname === "/about" ? "nav-link active" : "nav-link"
+          }
         >
           About
         </Link>
       </li>
       <li className="nav-item">
         <Link
-          to="/blog"
-          className={location.pathname === "/blog" ? "nav-link active" : "nav-link"}
+          to="/resume"
+          className={
+            location.pathname === "/resume" ? "nav-link active" : "nav-link"
+          }
         >
-          Blog
+          Resume
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/portfolio"
+          className={
+            location.pathname === "/portfolio" ? "nav-link active" : "nav-link"
+          }
+        >
+          Portfolio
         </Link>
       </li>
       <li className="nav-item">
         <Link
           to="/contact"
-          className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+          className={
+            location.pathname === "/contact" ? "nav-link active" : "nav-link"
+          }
         >
           Contact
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/contact/learn"
-          className={location.pathname === "/contact/learn" ? "nav-link active" : "nav-link"}
-        >
-          Learn
         </Link>
       </li>
     </ul>
