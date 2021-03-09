e5b41f5698c5cbbd8ba7b98135d2335c2a4319b7
Pavel Darii <paveldarii@yahoo.com>
2021-03-08 21:33:44 -0500

    chaged the description of the home page

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -7,8 +7,10 @@ function Hero() {
     <div id="hero-container">
       <img src={profileImg} alt="pavel darii" id="hero-img" />
       <div id="main-text-container">
-        <h1 className="main-text">Hello, I'm Pavel.</h1>
-        <h2 className="main-text">I'm Full Stack Web Developer</h2>
+        <h1 className="main-text">Hi, I'm Pavel, and</h1>
+        <h2 className="main-text">
+          I'm <span id="job-name">Web Developer</span>
+        </h2>
         <AnimatedText id="home-greeting"></AnimatedText>
       </div>
     </div>
