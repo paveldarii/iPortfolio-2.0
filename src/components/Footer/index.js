f849b2edd095f09b730dcb77b9c5031d4e205e9d
Pavel Darii <paveldarii@yahoo.com>
2021-03-11 17:22:45 -0500

    created a basic footer with three icons that leads to github linkedin and facebook

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,9 @@ import "./style.css";
 function Footer() {
   return (
     <footer className="footer">
-      <span>Pupster 2017</span>
+      <i className="fa fa-facebook"></i>
+      <i className="fa fa-linkedin"></i>
+      <i className="fa fa-github"></i>
     </footer>
   );
 }
