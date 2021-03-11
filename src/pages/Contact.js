10e99004861cf99e0dbacb81a3882b6f7a7387c2
Pavel Darii <paveldarii@yahoo.com>
2021-03-10 21:29:02 -0500

    added Contact page content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,19 +4,114 @@ function Contact(props) {
   return (
     <div>
       <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <section id="contact" class="contact">
+        <div className="container">
+          <div className="section-title">
+            <h2>Contact</h2>
+          </div>
+
+          <div className="row" data-aos="fade-in">
+            <div className="col-lg-5 d-flex align-items-stretch">
+              <div className="info">
+                <div className="address">
+                  <i className="icofont-google-map"></i>
+                  <h4>Location:</h4>
+                  <p>Columbus, Ohio</p>
+                </div>
+
+                <div className="email">
+                  <i className="icofont-envelope"></i>
+                  <h4>Email:</h4>
+                  <p>paveldarii@yahoo.com</p>
+                </div>
+
+                <div className="phone">
+                  <i className="icofont-phone"></i>
+                  <h4>Call:</h4>
+                  <p>+1 614 641 3785</p>
+                </div>
+
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24463.4363160339!2d-83.0199630513083!3d39.9653280248839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88388f2fc58bb6e5%3A0xdbbe8dd1f9c928f2!2sDowntown%2C%20Columbus%2C%20OH%2043215%2C%20USA!5e0!3m2!1sen!2sbg!4v1607481847015!5m2!1sen!2sbg"
+                  frameborder="0"
+                  title="contact"
+                  style="border:0; width: 100%; height: 290px;"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </div>
+
+            <div className="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+              <form
+                action="/api/send_email"
+                method="post"
+                role="form"
+                class="php-email-form"
+              >
+                <div className="form-row">
+                  <div className="form-group col-md-6">
+                    <label for="name">Your Name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      class="form-control"
+                      id="name"
+                      data-rule="minlen:4"
+                      data-msg="Please enter at least 4 chars"
+                    />
+                    <div className="validate"></div>
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label for="name">Your Email</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      name="email"
+                      id="email"
+                      data-rule="email"
+                      data-msg="Please enter a valid email"
+                    />
+                    <div className="validate"></div>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label for="name">Subject</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    name="subject"
+                    id="subject"
+                    data-rule="minlen:4"
+                    data-msg="Please enter at least 8 chars of subject"
+                  />
+                  <div className="validate"></div>
+                </div>
+                <div className="form-group">
+                  <label for="name">Message</label>
+                  <textarea
+                    className="form-control"
+                    name="message"
+                    rows="10"
+                    data-rule="required"
+                    data-msg="Please write something for us"
+                  ></textarea>
+                  <div className="validate"></div>
+                </div>
+                <div className="mb-3">
+                  <div className="loading">Loading</div>
+                  <div className="error-message"></div>
+                  <div className="sent-message">
+                    Your message has been sent. Thank you!
+                  </div>
+                </div>
+                <div className="text-center">
+                  <button type="submit">Send Message</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
