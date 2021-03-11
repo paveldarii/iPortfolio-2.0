664297e3fa6abb7919f2cab46859e833ed4c46f3
Pavel Darii <paveldarii@yahoo.com>
2021-03-11 17:24:49 -0500

    transformed class attribute to className

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Contact(props) {
   return (
     <div id="contact-page">
-      <section id="contact" class="contact">
+      <section id="contact" className="contact">
         <div className="section-title">
           <h2>Contact</h2>
         </div>
@@ -13,32 +13,32 @@ function Contact(props) {
             <div className="col-lg-4 offset-md-4  d-flex align-items-stretch">
               <div className="info">
                 <div className="email">
-                  <i class="fa fa-user"></i>
+                  <i className="fa fa-user"></i>
                   <h4>Name:</h4>
                   <p>Pavel Darii</p>
                 </div>
                 <div className="address">
-                  <i class="fa fa-map-marker"></i>
+                  <i className="fa fa-map-marker"></i>
                   <h4>Location:</h4>
                   <p>Columbus, Ohio</p>
                 </div>
                 <div className="email">
-                  <i class="fa fa-envelope"></i>
+                  <i className="fa fa-envelope"></i>
                   <h4>Email:</h4>
                   <p>paveldarii@yahoo.com</p>
                 </div>
 
                 <div className="phone">
-                  <i class="fa fa-phone"></i>
+                  <i className="fa fa-phone"></i>
                   <h4>Call:</h4>
                   <p>+1 614 641 3785</p>
                 </div>
 
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24463.4363160339!2d-83.0199630513083!3d39.9653280248839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88388f2fc58bb6e5%3A0xdbbe8dd1f9c928f2!2sDowntown%2C%20Columbus%2C%20OH%2043215%2C%20USA!5e0!3m2!1sen!2sbg!4v1607481847015!5m2!1sen!2sbg"
-                  frameborder="0"
+                  frameBorder="0"
                   title="contact"
-                  allowfullscreen
+                  allowFullScreen
                 ></iframe>
               </div>
             </div>
