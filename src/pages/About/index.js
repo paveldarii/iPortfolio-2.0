02e1138aeb8c9af15d4345bc3f8120ee3195cd60
Pavel Darii <paveldarii@yahoo.com>
2021-03-12 09:31:16 -0500

    removed ul items and inserted insted a paragraph text

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -11,39 +11,24 @@ function About() {
           </div>
 
           <div className="row">
-            <div className="col-lg-4" data-aos="fade-right">
+            <div className="col-lg-5" data-aos="fade-right">
               <img src={profileImg} className="img-fluid" alt="" />
             </div>
-            <div className="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <div className="col-lg-7 pt-3 pt-lg-0 content" data-aos="fade-left">
               <h3>Pavel Darii</h3>
               <p className="font-italic">Full-Stack Web Developer</p>
-              <div className="row">
-                <div className="col-lg-6">
-                  <ul>
-                    <li>
-                      <i className="icofont-rounded-right"></i>{" "}
-                      <strong>City:</strong> Columbus, Ohio
-                    </li>
-                    <li>
-                      <i className="icofont-rounded-right"></i>{" "}
-                      <strong>Degree: </strong> OSU Bootcamp Certificate in Web
-                      Development
-                    </li>
-                  </ul>
-                </div>
-                <div className="col-lg-6">
-                  <ul>
-                    <li>
-                      <i className="icofont-rounded-right"></i>{" "}
-                      <strong>Phone:</strong> 614-641-3785
-                    </li>
-                    <li>
-                      <i className="icofont-rounded-right"></i>{" "}
-                      <strong>Email:</strong> paveldarii@yahoo.com
-                    </li>
-                  </ul>
-                </div>
-              </div>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </p>
             </div>
           </div>
         </div>
