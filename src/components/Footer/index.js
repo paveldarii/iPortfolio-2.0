98e90a814ddb9ecb1d1acde1f9ef5cb8180fa34a
Pavel Darii <paveldarii@yahoo.com>
2021-03-12 00:36:11 -0500

    added icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,10 +3,24 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <i className="fa fa-facebook"></i>
-      <i className="fa fa-linkedin"></i>
-      <i className="fa fa-github"></i>
+    <footer className="social-links mt-3 text-center footer">
+      <a href="https://github.com/paveldarii" target="_blank" class="github">
+        <i className="fa fa-github fa-lg"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/pavel-darii-0aa546185/"
+        target="_blank"
+        className="linkedin"
+      >
+        <i className="fa fa-linkedin fa-lg"></i>
+      </a>
+      <a
+        href="https://www.facebook.com/pavel.darii"
+        target="_blank"
+        className="facebook"
+      >
+        <i className="fa fa-facebook fa-lg"></i>
+      </a>
     </footer>
   );
 }
