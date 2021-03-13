541b701c365ac8adbf458a038d0f6c7888d28d07
Pavel Darii <paveldarii@yahoo.com>
2021-03-12 20:21:17 -0500

    added a video as a holder for the future video that will describe me

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,36 +1,29 @@
 import React from "react";
 import "./style.css";
-import profileImg from "../../portfolio/profile-img.jpg";
+import Wrapper from "../../components/Wrapper";
 function About() {
   return (
     <div id="about-section">
       <section id="about" className="about">
         <div className="container">
-          <div className="section-title">
-            <h2>About</h2>
-          </div>
+          <div className="section-title"></div>
 
-          <div className="row">
-            <div className="col-lg-5" data-aos="fade-right">
-              <img src={profileImg} className="img-fluid" alt="" />
-            </div>
-            <div className="col-lg-7 pt-3 pt-lg-0 content" data-aos="fade-left">
-              <h3>Pavel Darii</h3>
-              <p className="font-italic">Full-Stack Web Developer</p>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
-            </div>
-          </div>
+          <Wrapper>
+            <h3>Pavel Darii</h3>
+          </Wrapper>
+          <Wrapper>
+            <p className="font-italic">Full-Stack Web Developer</p>
+          </Wrapper>
+          <Wrapper>
+            <iframe
+              width="760"
+              height="428"
+              src="https://www.youtube.com/embed/ZEyAs3NWH4A"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </Wrapper>
         </div>
       </section>
     </div>
